2c5427a96a57cefd3c144263574dce97a56a4381
Frits Hoogland <fhoogland@yugabyte.com>
2022-09-06 19:49:14 +0200

    updated tar.gz, updated sha256

--- Formula/yb_stats.rb
+++ Formula/yb_stats.rb
@@ -5,7 +5,7 @@ class YbStats < Formula
   desc "A utility to read all available meta-data that should be present in a standard YugabyteDB cluster"
   homepage "https://github.com/fritshoogland-yugabyte/yb_stats"
   url "https://github.com/fritshoogland-yugabyte/yb_stats/releases/download/v0.8.7/yb_stats.tar.gz"
-  sha256 "9dbdcf5ec8821e9c9ac14e89dc4d347b8df80e6c0c1244d2de75dfe4a6ad4965"
+  sha256 "480c978c8ba5c27a096523a22e74b2a389121538ab89fee8468a294a80dbbbd2"
   version "v0.8.7"
 
   def install
