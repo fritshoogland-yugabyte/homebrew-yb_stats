c7bd0de5f2a8603481fe221e7049b777fdbec065
Frits Hoogland <fhoogland@yugabyte.com>
2022-09-06 17:56:48 +0200

    capitalize class name

--- Formula/yb_stats.rb
+++ Formula/yb_stats.rb
@@ -4,7 +4,7 @@
 class YbStats < Formula
   desc "A utility to read all available meta-data that should be present in a standard YugabyteDB cluster"
   homepage "https://github.com/fritshoogland-yugabyte/yb_stats"
-  url "https://github.com/fritshoogland-yugabyte/yb_stats/releases/download/v0.8.7/yb_stats.tar.gz"
+  url "https://github.com/fritshoogland-yugabyte/yb_stats/releases/download/latest/yb_stats.tar.gz"
   sha256 "9dbdcf5ec8821e9c9ac14e89dc4d347b8df80e6c0c1244d2de75dfe4a6ad4965"
   version "0.8.7"
 
