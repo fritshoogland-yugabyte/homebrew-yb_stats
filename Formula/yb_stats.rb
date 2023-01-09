acd97ff15552205221c6754e62a3d33bfd5363e4
Frits Hoogland <fhoogland@yugabyte.com>
2023-01-09 19:41:25 +0100

    v0.9.1

--- Formula/yb_stats.rb
+++ Formula/yb_stats.rb
@@ -4,9 +4,9 @@
 class YbStats < Formula
   desc "A utility to read all available meta-data that should be present in a standard YugabyteDB cluster"
   homepage "https://github.com/fritshoogland-yugabyte/yb_stats"
-  url "https://github.com/fritshoogland-yugabyte/yb_stats/releases/download/v0.9.0/yb_stats-osx-intel-v0.9.0-1.tar.gz"
-  sha256 "ef04cff5b8240bf1a2209933d2bbf8713a36997f0e5fb93ea9b363b362b8e724"
-  version "v0.9.0"
+  url "https://github.com/fritshoogland-yugabyte/yb_stats/releases/download/v0.9.1/yb_stats-osx-intel-v0.9.1-1.tar.gz"
+  sha256 "7f47a3e17726a30e877f993974abec7dcb266728ebeccff70483aa2a66fbb961"
+  version "v0.9.1"
 
   def install
     bin.install "yb_stats"
